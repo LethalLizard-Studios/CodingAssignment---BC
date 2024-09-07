f97911ca3efdf6f14c6d15c2660a8739c7805e5d
Leland Carter <90154204+LethalLizard-Studios@users.noreply.github.com>
2024-09-06 21:55:26 -0300

    Update check and actions added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,46 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        int currentVersionYear = 2023;
+
+        List<Appliance> appliancesList = new ArrayList<Appliance>();
+
+        if (CheckIfUpdateRequired(currentVersionYear)) {
+            UpdateSystem(appliancesList);
+            currentVersionYear = LocalDate.now().getYear();
+        }
+
         System.out.println("Hello world!");
+
+    }
+
+    // Checks if current date is equal to the updates date and time or after and not yet updated
+    public static boolean CheckIfUpdateRequired(int currentVersionYear) {
+        LocalDate currentDate = LocalDate.now();
+        LocalTime currentTime = LocalTime.now();
+
+        //If current version is up-to-date or newer don't update
+        if (currentVersionYear >= currentDate.getYear())
+            return false;
+
+        // Update each year on January 1st 1:00am local time
+        LocalDate updateDate = LocalDate.of(currentDate.getYear(), 1, 1);
+        LocalTime updateTime = LocalTime.of(1, 0);
+
+        return !currentDate.isBefore(updateDate) && !currentTime.isBefore(updateTime);
+    }
+
+    public static void UpdateSystem(List<Appliance> appliancesList) {
+        // Force all appliances to turn off
+        for (Appliance appliance : appliancesList) {
+            appliance.forceOff();
+        }
     }
 }
